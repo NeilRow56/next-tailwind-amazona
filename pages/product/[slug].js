531377aad765964816f3adc503fc89c8187d88af
Neil <neilrowland56@yahoo.com>
2022-06-10 16:09:34 +0100

    Added react context for - add to cart- functionailty

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import { useRouter } from 'next/router';
 import data from '../../utils/data';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+	const { state, dispatch } = useContext(Store);
+
 	const { query } = useRouter();
 	const { slug } = query;
 	const product = data.products.find((x) => x.slug === slug);
 	if (!product) {
 		return <div>Product Not Found</div>;
 	}
+
+	const addToCartHandler = () => {
+		const existItem = state.cart.cartItems.find(
+			(x) => x.slug === product.slug
+		);
+		const quantity = existItem ? existItem.quantity + 1 : 1;
+
+		if (product.countInStock < quantity) {
+			alert('Sorry. Product is out of stock');
+			return;
+		}
+
+		dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+	};
+
 	return (
 		<Layout title={product.name}>
 			<div className="py-2">
@@ -55,7 +73,10 @@ export default function ProductScreen() {
 									: 'Unavailable'}
 							</div>
 						</div>
-						<button className="primary-button w-full">
+						<button
+							onClick={addToCartHandler}
+							className="primary-button w-full"
+						>
 							Add to cart
 						</button>
 					</div>
