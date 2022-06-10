ee807cf031fd8783be8842f4c3057e606bac2dcb
Neil <neilrowland56@yahoo.com>
2022-06-10 17:06:14 +0100

    Installed heroicons-react. Created cart.js - Remove items from cart- option for selecting product quantity

--- utils/Store.js
+++ utils/Store.js
@@ -1,11 +1,9 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { createContext, useReducer } from 'react';
-
 export const Store = createContext();
-
 const initialState = {
 	cart: { cartItems: [] },
 };
-
 function reducer(state, action) {
 	switch (action.type) {
 		case 'CART_ADD_ITEM': {
@@ -18,13 +16,19 @@ function reducer(state, action) {
 						item.name === existItem.name ? newItem : item
 				  )
 				: [...state.cart.cartItems, newItem];
+
+			return { ...state, cart: { ...state.cart, cartItems } };
+		}
+		case 'CART_REMOVE_ITEM': {
+			const cartItems = state.cart.cartItems.filter(
+				(item) => item.slug !== action.payload.slug
+			);
 			return { ...state, cart: { ...state.cart, cartItems } };
 		}
 		default:
 			return state;
 	}
 }
-
 export function StoreProvider({ children }) {
 	const [state, dispatch] = useReducer(reducer, initialState);
 	const value = { state, dispatch };
